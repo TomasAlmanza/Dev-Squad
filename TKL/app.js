e01d483d8e1e44dc33cc725af52e4b46c8c2d256
noeliafranco <noeluchi86@gmail.com>
2021-12-20 20:19:48 -0300

    subiendo html y css de login y regisro

--- TKL/app.js
+++ TKL/app.js
@@ -7,7 +7,19 @@ app.use (express.static(publicPath));
 
 app.get('/',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'./views/index.html'))
-})
+});
+
+app.get('/register',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/register.html'))
+});
+
+app.get('/login',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/login.html'))
+});
+
+app.get('/productDetail',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/productDetail.html'))
+});
 
 app.listen(3030,() => {
     console.log ('servidor corriendo ok')
